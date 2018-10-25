044bfe7cfbc1de0c78ece1204e5f435466d4c1e7
Tom Wood <tpw0007@uah.edu>
2018-10-25 13:09:07 -0500

    Got bored, so added bash scripts to execute on Linux Lab Machines

---
+++ run_client.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -cp ../ VIC.Simulation_Client "$1 $2"
---
+++ run_window.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -cp ../ VIC.Window_Publisher
